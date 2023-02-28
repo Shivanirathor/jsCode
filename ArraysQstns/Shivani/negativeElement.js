67a7d5a99f964d46c9938ee662996444ee6fdf8a
afrobug <afrojahmed6@gmail.com>
2023-03-01 01:07:22 +0530

    Revision

--- ArraysQstns/Shivani/negativeElement.js
+++ ArraysQstns/Shivani/negativeElement.js
@@ -1,18 +1,21 @@
-const arr = [-12, 11, -13, -5, 6, -7, 5, -3, -6];
+const arr = [-12, 11, -13, -5, 0, 6, -7, 5, -3, -6];
 
-const negElement=(arr)=>{
+const negElement = (arr) => {
     let result = [];
-    for(let i=0; i<arr.length; i++){
-        if(arr[i] < 0){
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 0) {
             result.push(arr[i]);
         }
     }
-    for(let i=0; i<arr.length; i++){
-        if(arr[i] >0){
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= 0) {
             result.push(arr[i]);
         }
     }
+    result.sort((a, b) => a - b);
     return result;
+
 }
 
 console.log(negElement(arr));
