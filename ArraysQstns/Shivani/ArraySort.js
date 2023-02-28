5a4124e322ade11360bdfb2e902721ed566b0186
Shivanirathor <Shivanirathor034@gmail.com>
2023-03-01 01:19:04 +0530

    arrayRotate

--- ArraysQstns/Shivani/ArraySort.js
+++ ArraysQstns/Shivani/ArraySort.js
@@ -2,13 +2,8 @@
 const arr1 = [22,4,1,6,4];
 const arr2 = [6,4,2,8,4];
 
-// let result = arr1+" "+ arr2;
-
-// for printing array we use concat
 const result = arr1.concat(arr2);
-//  console.log(result);
-
-result.sort((a,b)=>a-b);
-// result.sort((a,b)=>b-a);
+// result.sort((a,b)=>a-b);
+result.sort((a,b)=>b-a);
 
 console.log(result);
--- ArraysQstns/Shivani/kthSmallest.js
+++ ArraysQstns/Shivani/kthSmallest.js
@@ -1,9 +1,10 @@
 const arr = [7, 10, 4, 3, 20, 15]; 
 const K = 3
-// arr.sort((a,b) => a-b);
-// console.log(arr);
+arr.sort((a,b) => a-b);
+console.log(arr);
 
-// console.log(arr[K-1]);
+console.log(arr[K-1]);
+console.log(arr[arr.length - K]);
 
 // function ktSmallest(arr, K){
 //     arr.sort((a,b) => a-b);
@@ -12,9 +13,9 @@ const K = 3
 
 // arrow function
 
-const ktSmallest=(arr, K)=>{
-    arr.sort((a,b) => a-b);
-    return arr[K-1];
-}
+// const ktSmallest=(arr, K)=>{
+//     arr.sort((a,b) => a-b);
+//     return arr[K-1];
+// }
 
-console.log(ktSmallest(arr, K));
+// console.log(ktSmallest(arr, K));
