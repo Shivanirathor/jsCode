989cb1ea69aee2f728f98d1be5a8c518b104db07
Shivanirathor <Shivanirathor034@gmail.com>
2023-03-05 11:55:15 +0530

    counter

--- ArraysQstns/Shivani/String&NumberSepration.js
+++ ArraysQstns/Shivani/String&NumberSepration.js
@@ -15,13 +15,20 @@ const arr = [3, 6, "hi", 4, "hello"];
 const separatedNumberAndString = (arr) => {
   let arr1 = [];
   let arr2 = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (typeof arr[i] === "string") {
-      arr1.push(arr[i]);
+  //   for (let i = 0; i < arr.length; i++) {
+  //     if (typeof arr[i] === "string") {
+  //       arr1.push(arr[i]);
+  //     } else {
+  //       arr2.push(arr[i]);
+  //     }
+  //   }
+  arr.map((data) => {
+    if (typeof(data) === "string") {
+      arr1.push(data);
     } else {
-      arr2.push(arr[i]);
+      arr2.push(data);
     }
-  }
+  });
   return [arr1, arr2];
 };
 console.log(separatedNumberAndString(arr));
