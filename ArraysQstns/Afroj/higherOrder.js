13930cd460f63b512d57ee3f5f725480d7079142
afrobug <afrojahmed6@gmail.com>
2023-04-09 21:14:05 +0530

    sharedApproachesInClass

--- ArraysQstns/Afroj/higherOrder.js
+++ ArraysQstns/Afroj/higherOrder.js
@@ -1,3 +1,4 @@
+// approach 1
 function calculate(operation) {
     function sum(num1, num2) {
         const result = num1 + num2;
@@ -18,6 +19,9 @@ function calculate(operation) {
 }
 
 calculate("ADD")(5, 4);
+// when a func retun func is called higher order func
+// first () is returning a function definition 
+// first () is for 1st func and 2nd () is for 2nd func (which is inside 1st func as per the definition of higher order func)
 calculate("SUBTRACT")(5, 4);
 
 // approach 2
@@ -37,3 +41,5 @@ function calculate(operation) {
 
 calculate("ADD")(6, 4);
 calculate("SUBTRACT")(6, 4);
+
+// }) () IIFE immediately invoked 
